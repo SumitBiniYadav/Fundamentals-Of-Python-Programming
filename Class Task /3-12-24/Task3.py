4656a9e9e80ccaa2fde645c499504897a06405cd
Sumit Yadav <brijbinisy@gmail.com>
2025-01-02 20:08:16 +0530

    Add files via upload

---
+++ Class Task /3-12-24/Task3.py
@@ -0,0 +1,12 @@
+print("Without using third variable")
+
+a = int(input("Enter a Number : "))
+b = int(input("Enter a Number : "))
+
+a = a+b
+b = a-b
+a = a-b
+
+print("Swapped value of a is : ",a)
+print("Swapped value of b is : ", b)
+
---
+++ Class Task /3-12-24/years to month.py
@@ -0,0 +1,11 @@
+print("Years to month conversion")
+
+year = int(input("Enter Year : "))
+month = int(input("Enter Month : "))
+
+years = month / 12
+months = year * 12
+
+print("Month : ", months)
+print("Year : ", years)
+
